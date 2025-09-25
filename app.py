22fbbfef28910149860fc4fe3206b0d4f0c10ad2
Shivam . <shi001@on.sinch.com>
2025-09-25 14:24:08 +0530

    Added hosted site web socket address

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
 from flask import Flask, request, jsonify
 import asyncio
 import websockets
+import io
+import sounddevice as sd
+import soundfile as sf
 
 app = Flask(__name__)
+TTS_SERVER_URI = "wss://tts-executer.onrender.com/ws"
 
-TTS_SERVER_URI = "ws://localhost:8765"  # Update this if your TTS server is hosted externally
-
-async def send_text_to_tts(text):
-    async with websockets.connect(TTS_SERVER_URI, max_size=None) as websocket:
+async def send_text_and_play_audio(text):
+    async with websockets.connect(TTS_SERVER_URI) as websocket:
         await websocket.send(text)
+        audio_bytes = await websocket.recv()
+
+        audio_file = io.BytesIO(audio_bytes)
+        data, samplerate = sf.read(audio_file, dtype='float32')
+        sd.play(data, samplerate)
+        sd.wait()
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
     payload = data.get("payload", {})
     text = payload.get("text", "")
-    sender_fullname = payload.get("senderFullname", "")
+    sender = payload.get("senderFullname", "")
 
-    if sender_fullname != "tiledesk-tts":
-        return jsonify({"status": "ignored", "message": "Sender not authorized"}), 200
+    print(f"📩 Received from {sender}: {text}")
+
+    if sender != "tiledesk-tts":
+        return jsonify({"status": "ignored"}), 200
 
     if not text:
-        return jsonify({"status": "error", "message": "No text found"}), 400
+        return jsonify({"status": "error", "message": "No text"}), 400
 
-    asyncio.run(send_text_to_tts(text))
-    return jsonify({"status": "success", "message": "Text forwarded to TTS server"}), 200
+    asyncio.run(send_text_and_play_audio(text))
+    return jsonify({"status": "success"}), 200
 
 if __name__ == "__main__":
-    print("✅ Flask server is up and running on http://localhost:5000")
-    app.run(host="0.0.0.0",port=5000)
+    app.run( port=5000)
